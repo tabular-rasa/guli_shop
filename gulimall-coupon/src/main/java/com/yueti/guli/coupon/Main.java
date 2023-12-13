631b35cfa72a9f7ff6132bf261de737752d64f96
sirius <747777389@qq.com>
2023-12-13 23:11:26 +0800

    结构初步

---
+++ gulimall-coupon/src/main/java/com/yueti/guli/coupon/Main.java
@@ -0,0 +1,7 @@
+package com.yueti.guli.coupon;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ gulimall-order/src/main/java/com/yueti/guli/order/Main.java
@@ -0,0 +1,7 @@
+package com.yueti.guli.order;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
